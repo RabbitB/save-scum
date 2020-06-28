d31f8b42e84ddf04acdb023f0fa72202f5377de5
Rabbit <rabbit@dfaction.net>
2020-06-28 14:01:37 -0700

    Simplify ScumDB
    Reduces the amount of info the ScumDB needs to track.

--- Project/global/scum_db.gd
+++ Project/global/scum_db.gd
@@ -5,20 +5,15 @@ const CONFIG_FILE_PATH: String = "user://scum_db.cfg"
 
 const GROUP_NAME_ID: String = "identifiers"
 const KEY_GAMES: String = "games"
-const KEY_SNAPSHOTS: String = "snapshots"
 const KEY_RECYCLED_GAMES: String = "recycled_games"
-const KEY_RECYCLED_SNAPSHOTS: String = "recycled_snapshots"
 
 var _config_file: ConfigFile = ConfigFile.new()
 var _last_used_game_id: int
-var _last_used_snapshot_id: int
 var _recycled_game_ids: Array
-var _recycled_snapshot_ids: Array
 
 
 func _init() -> void:
 	_recycled_game_ids = []
-	_recycled_snapshot_ids = []
 
 
 func _ready() -> void:
@@ -34,12 +29,10 @@ func load_scum_db() -> void:
 	if error:
 		Log.error("Error loading the file db. Error code %s. To preserve existing saves, cannot continue." \
 				 % [Log.get_error_description(error)])
-		get_tree().quit(1)
+		get_tree().quit(FAILED)
 	else:
 		_last_used_game_id = _config_file.get_value(GROUP_NAME_ID, KEY_GAMES, -1)
-		_last_used_snapshot_id = _config_file.get_value(GROUP_NAME_ID, KEY_SNAPSHOTS, -1)
 		_recycled_game_ids = _config_file.get_value(GROUP_NAME_ID, KEY_RECYCLED_GAMES, [])
-		_recycled_snapshot_ids = _config_file.get_value(GROUP_NAME_ID, KEY_RECYCLED_SNAPSHOTS, [])
 
 
 func get_new_game_id() -> int:
@@ -56,29 +49,9 @@ func get_new_game_id() -> int:
 	return new_game_id
 
 
-func get_new_snapshot_id() -> int:
-	var new_snapshot_id: int
-	if _recycled_snapshot_ids.size() > 0:
-		new_snapshot_id = _recycled_snapshot_ids.pop_front()
-		_config_file.set_value(GROUP_NAME_ID, KEY_RECYCLED_SNAPSHOTS, _recycled_snapshot_ids)
-	else:
-		_last_used_snapshot_id += 1
-		new_snapshot_id = _last_used_snapshot_id
-		_config_file.set_value(GROUP_NAME_ID, KEY_SNAPSHOTS, _last_used_snapshot_id)
-
-	_save_scum_db()
-	return new_snapshot_id
-
-
 func recycle_game_id(recycle_id: int) -> void:
 	_recycled_game_ids.append(recycle_id)
 	_config_file.set_value(GROUP_NAME_ID, KEY_RECYCLED_GAMES, _recycled_game_ids)
-	_save_scum_db()
-
-
-func recycle_snapshot_id(recycle_id: int) -> void:
-	_recycled_snapshot_ids.append(recycle_id)
-	_config_file.set_value(GROUP_NAME_ID, KEY_RECYCLED_SNAPSHOTS, _recycled_snapshot_ids)
 	_save_scum_db()
 
 
@@ -87,5 +60,5 @@ func _save_scum_db() -> void:
 	if error:
 		Log.error("Error saving the file db. Error code %s. To preserve existing saves, cannot continue." \
 				% [Log.get_error_description(error)])
-		get_tree().quit(1)
+		get_tree().quit(FAILED)
 
