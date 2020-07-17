a55be38ea7035572960dbe39457ec9a91f4657e4
Rabbit <rabbit@dfaction.net>
2020-07-17 14:51:37 -0700

    Modify DraggableNode to use MovableNode
    Updated DraggableNode (again) to use MovableNode for its handle and target. Now DraggableNode doesn't care what you give it, as long as the handle and target are either based on Node2D or Control. Regardless of exactly what node you want to use, they should all behave similarly now, instead of the slight difference between them before.

--- Project/ui/draggable_node/draggable_node.gd
+++ Project/ui/draggable_node/draggable_node.gd
@@ -18,13 +18,15 @@ enum DRAG_EVENTS {
 	ENDED
 }
 
+const MovableNode: Script = preload("res://ui/draggable_node/movable_node.gd")
+
 export(NodePath) var handle_path: NodePath setget _set_handle_path
 export(NodePath) var target_path: NodePath setget _set_target_path
 export(Rect2) var handle_area: Rect2 = Rect2(0, 0, 16, 16) setget _set_handle_area
 export(MODIFIER_KEYS) var hold_key_to_drag: int = MODIFIER_KEYS.NONE
 
-var _handle: Node2D
-var _target: Node2D
+var _handle: MovableNode
+var _target: MovableNode
 
 var _is_dragging: bool
 var _mouse_to_origin_offset: Vector2
@@ -46,13 +48,18 @@ func _input(event: InputEvent) -> void:
 		var can_drag: bool = !hold_key_to_drag || Input.is_key_pressed(hold_key_to_drag)
 
 		if can_drag && mouse_event.pressed && mouse_event.button_index == BUTTON_LEFT:
-			var draggable_area: Rect2 = (_handle as Sprite).get_rect() if _handle is Sprite else handle_area
+			var draggable_area: Rect2 = handle_area
+
+			if _handle.node_to_move is Sprite:
+				draggable_area = (_handle.node_to_move as Sprite).get_rect()
+			elif _handle.node_to_move is Control:
+				draggable_area = (_handle.node_to_move as Control).get_rect()
 
 			#	CollisionObject2D feeds input through a signal, so we handle it elsewhere.
-			if !(_handle is CollisionObject2D):
-				if draggable_area.has_point(_handle.get_local_mouse_position()):
+			if !(_handle.node_to_move is CollisionObject2D):
+				if draggable_area.has_point(_handle.node_to_move.get_local_mouse_position()):
 					_is_dragging = true
-					_mouse_to_origin_offset = _handle.global_position - get_global_mouse_position()
+					_mouse_to_origin_offset = _target.global_position - get_global_mouse_position()
 
 					emit_signal("dragged", DRAG_EVENTS.STARTED)
 					get_tree().set_input_as_handled()
@@ -76,7 +83,13 @@ func _input(event: InputEvent) -> void:
 func _draw() -> void:
 	_update_handle()
 
-	if Engine.editor_hint && _handle != null && !(_handle is Sprite || _handle is CollisionObject2D):
+	if !Engine.editor_hint || !_handle:
+		return
+
+	var node_to_move: Node = _handle.node_to_move
+	var drawable_type: bool = !(node_to_move is Sprite || node_to_move is Control || node_to_move is CollisionObject2D)
+
+	if drawable_type:
 		draw_rect(handle_area, Color.violet, false, 1.0, true)
 
 
@@ -85,32 +98,41 @@ func _get_configuration_warning() -> String:
 	_update_target()
 
 	if !_handle && !_target:
-		return "Both the handle and target must be a child of Node2D or a class that extends Node2D."
+		return "Both the handle and target must extend or be a child of, Node2D or Control."
 
 	if !_handle:
-		return "The handle must be a child of Node2D or a class that extends Node2D."
+		return "The handle must extend or be a child of, Node2D or Control."
 
 	if !_target:
-		return "The target must be a child of Node2D or a class that extends Node2D."
+		return "The target must extend or be a child of, Node2D or Control."
 
 	return ""
 
 
 func _on_parent_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
+		_update_handle()
+		_update_target()
+
+		if !_handle || !_target:
+			return
+
 		var mouse_event: InputEventMouseButton = event as InputEventMouseButton
 		_is_dragging = mouse_event.pressed && mouse_event.button_index == BUTTON_LEFT
+
+		if _is_dragging:
+			_mouse_to_origin_offset = _target.global_position - get_global_mouse_position()
 
 
 func _update_handle() -> void:
 	if _handle == null && !handle_path.is_empty():
-		_handle = get_node(handle_path)
+		_handle = MovableNode.new(get_node(handle_path))
 
-		if !(_handle is Node2D):
+		if _handle.node_to_move == null:
 			_handle = null
-		elif _handle is CollisionObject2D:
+		elif _handle.node_to_move is CollisionObject2D:
 # warning-ignore:return_value_discarded
-			_handle.connect("input_event", self, "_on_handle_input_event")
+			_handle.node_to_move.connect("input_event", self, "_on_handle_input_event")
 
 		if Engine.editor_hint:
 			update()
@@ -118,17 +140,17 @@ func _update_handle() -> void:
 
 func _update_target() -> void:
 	if _target == null && !target_path.is_empty():
-		_target = get_node(target_path)
+		_target = MovableNode.new(get_node(target_path))
 
-		if !(_target is Node2D):
+		if _target.node_to_move == null:
 			_target = null
 
 
 func _set_handle_path(value: NodePath) -> void:
 	handle_path = value
 
-	if _handle && _handle is CollisionObject2D:
-		_handle.disconnect("input_event", self, "_on_handle_input_event")
+	if _handle && _handle.node_to_move is CollisionObject2D:
+		_handle.node_to_move.disconnect("input_event", self, "_on_handle_input_event")
 
 	_handle = null
 	update_configuration_warning()
@@ -136,8 +158,8 @@ func _set_handle_path(value: NodePath) -> void:
 
 func _set_target_path(value: NodePath) -> void:
 	target_path = value
-
 	_target = null
+
 	update_configuration_warning()
 
 
